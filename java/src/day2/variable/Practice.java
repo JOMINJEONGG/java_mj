b6e8e4d3b9141d7c797bec76a3c06fbc625f463b
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-05-31 12:41:27 +0900

    자바 변수 연습문제

---
+++ java/src/day2/variable/Practice.java
@@ -0,0 +1,18 @@
+package day2.variable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		//1. 
+		//char ch1 = "A" - 잘못된 부분을 찾아서 수정하시오
+	    //문자는  ''로 감싸여야한다. "" 아님
+		char ch1 = 'A';
+		/* 2.
+		 * 국어, 영어, 수학 성적을 입력하여 평균을 구하려한다. 이 떄 필요한 변수들을 선언하세요
+		 * */
+		int kor, eng, math; // 성적이 정수로 나온다는 가정하에 int로 변수 선언
+		double avg; // 평균을 저장할 변수(소수점이 있을 수 있기 때문에 double 타입으로 선언, float로 사용해도 됨) avg = average약자
+
+	}
+
+}
