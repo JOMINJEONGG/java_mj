76307cecca27e011b46879f76248de58c426c200
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-02 08:45:50 +0900

    자바 : dat3 숙제

--- java/src/day3/homework/Ex1_conditional.java
+++ java/src/day3/homework/Ex1_conditional.java
@@ -1,10 +1,72 @@
 package day3.homework;
 
+
+
+import java.util.Scanner;
+
+
+
 public class Ex1_conditional {
 
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*성별(M:남자,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건연산자 이용
+
+		 * input gender : 
+
+		 * M
+
+		 * Are you a women? false
+
+		 * input gender : 
+
+		 * W
+
+		 * Are you a women? true
+
+		 * */
+
+		
+
+		Scanner sc = new Scanner(System.in);
+
+		
+
+		System.out.println("input gender : ");
+
+		char gender = sc.next().charAt(0);
+
+		
+
+		String result = gender == 'W' ? "true" : "false";
+
+		System.out.println("Are you a women? " + result);
+
+		
+
+		System.out.println("input gender : ");
+
+		char gender2 = sc.next().charAt(0);
+
+		
+
+		String result2 = gender == 'M' ? "true" : "false";
+
+		System.out.println("Are you a women? " + result2
+
+				);
+
+		
+
+		sc.close();
+
+
 
 	}
 
+
+
 }
+
--- java/src/day3/homework/Ex2_if.java
+++ java/src/day3/homework/Ex2_if.java
@@ -1,10 +1,82 @@
 package day3.homework;
 
+
+
+import java.util.Scanner;
+
+
+
 public class Ex2_if {
 
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용
+
+		 * input gender : 
+
+		 * M
+
+		 * Are you a women? man
+
+		 * input gender : 
+
+		 * W
+
+		 * Are you a women? woman
+
+		 * */
+
+		
+
+		Scanner sc = new Scanner(System.in);
+
+		String M = "man";
+
+		String W = "woman";
+
+		
+
+		System.out.println("input gender : ");
+
+		String string = sc.nextLine(); 
+
+		String result = "man" == M ? "man" : "woman";
+
+		if(M == "man") {
+
+			System.out.println("M");
+
+		}
+
+		System.out.println("Are you a women? " + result);
+
+		
+
+		System.out.println("input gender : ");
+
+		String string2 = sc.nextLine(); 
+
+		String result2 = "woman" == W ? "woman" : "man";
+
+		if(W == "woman") {
+
+			System.out.println("W");
+
+		}
+
+		System.out.println("Are you a women? " + result2);
+
+		
+
+		sc.close();
+
+
 
 	}
 
+
+
 }
+
