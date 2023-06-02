662a1e06dc2b5f69e27af0432a05d3180e541f2e
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-02 14:34:36 +0900

    자바 : 3일차 과제 내가 한 것

--- java/src/day3/homework/Ex1_conditional.java
+++ java/src/day3/homework/Ex1_conditional.java
@@ -1,72 +1,37 @@
 package day3.homework;
 
-
-
 import java.util.Scanner;
-
-
 
 public class Ex1_conditional {
 
-
-
 	public static void main(String[] args) {
-
 		/*성별(M:남자,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건연산자 이용
-
 		 * input gender : 
-
 		 * M
-
 		 * Are you a women? false
-
 		 * input gender : 
-
 		 * W
-
 		 * Are you a women? true
-
 		 * */
-
-		
+		//내가한것
 
 		Scanner sc = new Scanner(System.in);
 
-		
-
 		System.out.println("input gender : ");
-
 		char gender = sc.next().charAt(0);
 
-		
-
 		String result = gender == 'W' ? "true" : "false";
-
 		System.out.println("Are you a women? " + result);
-
-		
-
+	
 		System.out.println("input gender : ");
-
 		char gender2 = sc.next().charAt(0);
 
-		
-
 		String result2 = gender == 'M' ? "true" : "false";
-
-		System.out.println("Are you a women? " + result2
-
-				);
-
-		
+		System.out.println("Are you a women? " + result2);	
 
 		sc.close();
 
-
-
 	}
-
-
 
 }
 
