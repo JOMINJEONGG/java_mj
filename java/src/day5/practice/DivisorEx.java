6f136b5316c63abcda3dd0a7fe6e175344c63ef1
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-05 15:01:43 +0900

    자바 : while문 소수 예제

---
+++ java/src/day5/practice/DivisorEx.java
@@ -0,0 +1,36 @@
+package day5.practice;
+
+import java.util.Scanner;
+
+public class DivisorEx {
+
+	public static void main(String[] args) {
+		/* 정수 num를 입력받아 num의 약수를 출력하는 코드를 작성하세요.
+		 * 약수 A를 B로 나누었을 때 나머지가 0이면 B는 A의 약수이다.
+		 * 12의 약수 : 1, 2, 3, 4, 6, 12
+		 * */
+		/* 반복횟수 : 1부터 num까지 1씩증가
+		 * 규칙성 : i가 num의 약수이면 i를 출력 
+		 * 		=>num를 i로 나누었을때 나머지가 0과 같다면 i를 출력
+		 * */
+		
+		int num, i; 
+		Scanner sc = new Scanner(System.in);
+
+		//정수 입력
+		System.out.println("input positive number : ");
+		num = sc.nextInt();
+
+		//반복문, i는 1부터 num까지 1씩 증가
+		i = 1;// i를 초기화
+		while(i <= num) {
+			//num를 i로 나누었을 때 나머지가 0과 같다면 i를 출력
+			if(num % i == 0) {
+			System.out.println(i);
+			}
+			++i;
+		}		
+		sc.close();
+	}
+
+}
