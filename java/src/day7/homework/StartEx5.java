11543cc6799c3bd4b6e26590059df60881d6560e
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-08 14:50:14 +0900

    자바 7일차 숙제

---
+++ java/src/day7/homework/StartEx5.java
@@ -0,0 +1,18 @@
+package day7.homework;
+
+public class StartEx5 {
+
+	public static void main(String[] args) {
+		/* 	  *			i=1 공백=3	 *=0
+		 *   ***		i=2 공백=2	 *=1
+		 *  *****		i=3 공백=1	 *=2
+		 * *******		i=4 공백=0	 *=3
+		 *  *****		i=3 공백=1	 *=2
+		 *   ***		i=2 공백=2	 *=1
+		 *    *			i=1 공백=3	 *=0
+		 * 이와 같이 출력되도록 반복문을 이용해보자
+		 * */
+
+		
+	}
+}
