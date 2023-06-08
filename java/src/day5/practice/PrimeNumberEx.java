924f1f06f398c510d54d292cab2c1e5c111d2d58
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-08 23:19:38 +0900

    주석 수정

--- java/src/day5/practice/PrimeNumberEx.java
+++ java/src/day5/practice/PrimeNumberEx.java
@@ -6,10 +6,10 @@ public class PrimeNumberEx {
 
 	public static void main(String[] args) {
 		/* 정수 num를 입력받아 num가 소수인지 아닌지를 판별하는 코드를 작성하세요
-		 * 소수 : 약수가 1과 자기 자신밖에 없는 수 => 약수가 2개인 수, 1을 제외하고 처음구한 약수가 자신인 수 
+		 * 소수 : 약수가 1과 자기 자신밖에 없는 수 => '약수가 2개인 수'이걸 이용해서 풀것임, 1을 제외하고 처음구한 약수가 자신인 수 
 		 * 소수ex : 2, 3, 5, 7, 11, 13 등
 		 * */
-		/* 반복성 : i는 1부터 num까지 1씩증가
+		/* 반복성 : i는 1부터 num까지 1씩 증가
 		 * 규칙성 : i가 num의 약수이면 약수의 개수 count를 1증가
 		 *  =>num를 i로 나누었을때 나머지가 0과 같다면 약수의 개수 count를 1증가
 		 *  반복문 종료 후 : count가 2이면 소수라고 출력, 아니면 소수가 아님이라고 출력 
@@ -30,7 +30,7 @@ public class PrimeNumberEx {
 			//count가 2이면 소수라고 출력, 아니면 소수가 아님이라고 출력
 			++i;
 		}
-		if(count ==2) {
+		if(count == 2) {
 			System.out.println(num + " is a prime number.");
 		}else {
 			System.out.println(num + " is not a prime number.");
