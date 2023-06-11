f773b7446945e8a5e5ef435bc166355549dba97c
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-12 00:03:13 +0900

    주석수정

--- java/src/day8/method/MethodCallStackEx.java
+++ java/src/day8/method/MethodCallStackEx.java
@@ -22,6 +22,6 @@ public class MethodCallStackEx {
 	public static void method4() {
 		System.out.println("a");
 		method4();
-	}//'재귀메서드' 스택이라는 공간에 꽉찰때까지 자기 자신을 계속 실행된다 => StacKOverflower발생
+	}//'재귀메서드' 스택이라는 공간에 꽉찰때까지 자기 자신을 계속 실행된다 => StackOverflower발생
 	//쌓이기 때문에 메서드가 종료했을 때 다른 메서드로 계속 쌓이지 않도록 해야함
 }
