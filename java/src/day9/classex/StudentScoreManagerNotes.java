4c8ea52f2bdb4c5395c26e641c2d61b037c2fd43
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-12 14:34:59 +0900

    자바 학생클래스

---
+++ java/src/day9/classex/StudentScoreManagerNotes.java
@@ -0,0 +1,40 @@
+package day9.classex;
+
+public class StudentScoreManagerNotes {
+
+	public static void main(String[] args) {
+		/*현실에 있는 정보		 =>		 클래스
+		프로그램을 통해 현실에 있는 정보를 관리하기 위해서 
+
+		<<고등학생 성적 관리 프로그램>>
+	
+		학생정보			=> Student
+		이름(0)			=> String name
+		주민번호(0)		=> String residentNumber
+		학년(0)			=> int grade
+		반(0)			=> int classNum
+		번호(0)			=> int number
+		나이(X) 			
+		생일(X)			
+		가족관계(X)		
+		주소(X)			
+		입학년도(0)		=> int admissionYear
+		1학년 1학기 국어성적	=> int korYear, korGrade, korScore(3개이면 비효율적)
+		1학년 2학기 국어성적	 
+		1학년 1학기 수학성적	
+						===>Score korScore, engScore, mathScore(아래 class만든 후 이렇게하기!)
+		등
+		...
+		
+		<성적			=> Score class로 만들기>
+		과목명		=> String title
+		학년			=> int grade
+		학기			=> int semester
+		총점수		=> double total
+		중간			=> double mid
+		기말			=> double finalExam
+		수행평가		=> double performance*/
+
+	}
+
+}
