6ce6148a7634e47991aec7530d63ade4fee96335
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-16 12:41:05 +0900

    자바 로또 과제 풀이

--- java/src/day12/practice/VocabularyNoteEx2.java
+++ java/src/day12/practice/VocabularyNoteEx2.java
@@ -9,6 +9,34 @@ public class VocabularyNoteEx2 {
 				new Word("def", "def os define")
 		};
 		VocabularyNote note = new VocabularyNote(wordList);
+		//note.print();
+		
+		//1)밖에서 단어와 뜻을 받아서 넘겨주는것
+		Word word = new Word("a", "a");
+		note.insert(word);;
+		//note.print();
+		
+		//2)단어와 뜻을 직접 넘겨주는 것
+		note.insert("b","b is b");;
+		//note.print();
+		
+		//note.delete("c");//No words found 출력
+		note.delete("a");
+		//note.print();
+		
+		//note.search("a");
+		//note.search("b");
+
+		note.insert("b", "b");
+		//note.print();
+		
+		note.updateMeaning("b", 2, "B");
+		note.updateMeaning("b", 3, "B");//Wrong number
+		//note.print();
+		
+		note.updateTitle("b", "B");
+		//No words found 출력. 이미 앞에서 수정해서 b가 없음
+		note.updateTitle("b", "B");
 		note.print();
 	}
 }
