1654064f4ce11ad84e9fd0aeaa67ee69a8d52cc3
JOMINJEONGG <134986690+JOMINJEONGG@users.noreply.github.com>
2023-06-26 15:11:39 +0900

    자바 : 산술연산 프로그램 과제 예제(18일차 과제1)

---
+++ java/src/day18/homework/arithmetic/controller/ArithmeticProgram.java
@@ -0,0 +1,7 @@
+package day18.homework.arithmetic.controller;
+
+public interface ArithmeticProgram {
+		void run();
+		void printMenu();
+		void runMenu(int menu);
+}
---
+++ java/src/day18/homework/arithmetic/run/ArithmeticRun.java
@@ -0,0 +1,13 @@
+package day18.homework.arithmetic.run;
+
+import day18.homework.arithmetic.controller.ArithmeticEx;
+
+public class ArithmeticRun {
+
+	public static void main(String[] args) {
+
+		ArithmeticEx ex = new ArithmeticEx();
+		ex.run();
+	}
+
+}
